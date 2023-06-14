f8bd1b4df246d57e752c6ac15272d29c34d049e1
jerry0509 <117144353+jerry0509@users.noreply.github.com>
2023-06-14 20:31:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getBuildConfig } from "./config/build";
 import { getClientConfig } from "./config/client";
 
 export const metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Yijun's ChatGPT Web",
+  description: "Yijun customized ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Yijun's ChatGPT Web",
     statusBarStyle: "default",
   },
 };
